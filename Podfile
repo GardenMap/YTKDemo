46b948348ee510d067cd008c6be23e7a582a026b
chenkai <chenkai@xitu.io>
2016-10-10 10:36:31 +0800

    增加YTKNetwork 框架引用

---
+++ Podfile
@@ -0,0 +1,4 @@
+target ‘YTKPackageDemo’ do
+platform:ios,’8.0’
+pod 'YTKNetwork'
+end
---
+++ Pods/Target Support Files/Pods-YTKPackageDemo/Pods-YTKPackageDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YTKPackageDemo : NSObject
+@end
+@implementation PodsDummy_Pods_YTKPackageDemo
+@end
